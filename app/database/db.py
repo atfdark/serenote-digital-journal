f99361a80f8c88576692aec19219d1d5c130f187
atfdark <147408184+atfdark@users.noreply.github.com>
2025-10-05 23:02:52 +0530

    completed

--- app/database/db.py
+++ app/database/db.py
@@ -2,17 +2,17 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker, declarative_base
 
-# --- Start of Changes ---
+# Use DATABASE_URL from environment, fallback to SQLite for local development
+db_uri = os.environ.get('DATABASE_URL', 'sqlite:///serenote.db')
 
-# For Vercel deployment, use /tmp for writable database
-db_path = os.path.join('/tmp', 'serenote.db')
-db_uri = 'sqlite:///' + db_path
+# Create engine based on database type
+if db_uri.startswith('sqlite'):
+    from sqlalchemy.pool import StaticPool
+    engine = create_engine(db_uri, echo=False, poolclass=StaticPool, pool_pre_ping=True)
+else:
+    # For PostgreSQL and other databases
+    engine = create_engine(db_uri, echo=False, pool_pre_ping=True)
 
-# --- End of Changes ---
-
-# Now use the absolute path URI to create the engine
-from sqlalchemy.pool import StaticPool
-engine = create_engine(db_uri, echo=False, poolclass=StaticPool, pool_pre_ping=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
